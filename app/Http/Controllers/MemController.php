68cab729957e620e0775c386c6b5ea75f71e3d57
AlvaroCori <bacs200018@gmail.com>
2025-04-07 20:43:38 -0400

    adding mem controller

--- app/Http/Controllers/MemController.php
+++ app/Http/Controllers/MemController.php
@@ -8,6 +8,8 @@ use Illuminate\Support\Facades\Http;
 use Illuminate\Validation\ValidationException;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Validator;
+use Carbon\Carbon;
+use Carbon\CarbonPeriod;
 class MemController extends Controller
 {
     private function store(Request $request)
@@ -47,9 +49,14 @@ class MemController extends Controller
     #get the index of array keys that are not in database
     private function exclude_current_keys($keys)
     {
+        $indexs = array();
         $currents = ApiHelper::getAlloweds(Mem::class, all: true);
-        $all_keys = MemController::get_keys($currents);
-        return array_diff($keys, $all_keys);
+        $all_keys = MemController::get_keys(data: $currents);
+        $dif_keys = array_diff($keys, $all_keys);
+        foreach ($dif_keys as $key) {
+            array_push($indexs, array_search($key, $keys));
+        }
+        return $indexs;
     }
     private function save_new_mems($data){
         DB::beginTransaction();
@@ -76,32 +83,85 @@ class MemController extends Controller
                 $count = $count + 1;
             }
             DB::commit();
-            return response()->json(['message' => "{$count} registros guardados exitosamente"], 201);
+            return $count;
         } catch (\Exception $e) {
             DB::rollBack();
-            return response()->json(['error' => $e->getMessage()], 400);
+            throw $e;
         }
     }
-    public function migrate(Request $request)
+    private function migrate_per_station_and_date($station_id, $date)
     {
-        $perPage = $request->input('items', 100);
-        $response = Http::withHeaders([
-            'Authorization' => 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpYXQiOjE3NDEzNTgyMjcsImV4cCI6MTgwNDQzMDIyN30.bErK9dnooNLn45MOzOEKNxq2Epbq3usdTTjWivbVvUk',
-            'Accept' => 'application/json',
-        ])->get('https://sr.info.bo/pro/dato_fecha/4/2025-04-06');
-        if ($response->successful()) {
-            $datos = $response->json();
-            $data = $datos['DATA'];
-            $new_indexs = MemController::exclude_current_keys(MemController::get_keys($data));
-            $new_objects = array();
-            foreach ($new_indexs as $index){
-                array_push($new_objects, $data[$index]);
+        try {
+            $response = Http::withHeaders([
+                'Authorization' => 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpYXQiOjE3NDEzNTgyMjcsImV4cCI6MTgwNDQzMDIyN30.bErK9dnooNLn45MOzOEKNxq2Epbq3usdTTjWivbVvUk',
+                'Accept' => 'application/json',
+            ])->get("https://sr.info.bo/pro/dato_fecha/{$station_id}/{$date}");
+            if ($response->successful()) {
+                $datos = $response->json();
+                $data = $datos['DATA'];
+                $new_indexs = MemController::exclude_current_keys(MemController::get_keys($data));
+                $new_objects = array();
+                foreach ($new_indexs as $index){
+                    array_push($new_objects, $data[$index]);
+                }
+                return MemController::save_new_mems($new_objects);
+            } else {
+                //$codigoError = $response->status();
+                $mensajeError = $response->body();
+                throw new \Exception($mensajeError);
             }
-            return MemController::save_new_mems($new_objects);
-        } else {
-            $codigoError = $response->status();
-            $mensajeError = $response->body();
-            return response()->json($mensajeError, $codigoError);
+        } catch (\Exception $e) {
+            throw $e;
+        }
+    }
+    public function migrate_per_day(Request $request)
+    {
+        $station_id = $request->input('estacion');
+        $date = $request->input('fecha');
+        if ($station_id == null){
+            return response()->json(['error' => 'Debes agregar un id en estacion'], 400);
+        }
+        if ($date == null){
+            return response()->json(['error' => 'Debes agregar una fecha'], 400);
+        }
+        try {
+            $count = MemController::migrate_per_station_and_date($station_id, $date);
+            return response()->json([
+                'exito' => "Se creo {$count} nuevos elementos."
+            ], 201);
+        } catch (\Exception $e) {
+            return response()->json([
+                'message' => 'Error al crear.',
+                'errors' => $e->getMessage(),
+            ], 404);
+        }
+    }
+    
+    public function migrate()
+    {
+        try {
+            $count = 0;
+            $stations_id = array(4,5, 6);
+            $init = Carbon::create(2025, 4, 3);
+            $now = Carbon::now();
+            $periods = CarbonPeriod::create($init, '1 day', $now);
+            $dates = [];
+            foreach ($periods as $date) {
+                array_push($dates, $date->format('Y-m-d'));
+            }
+            foreach($stations_id as $station_id){
+                foreach($dates as $date){
+                    $count = $count + MemController::migrate_per_station_and_date($station_id, $date);
+                }
+            }
+            return response()->json([
+                'exito' => "Se creo {$count} nuevos elementos."
+            ], 201);
+        } catch (\Exception $e) {
+            return response()->json([
+                'message' => 'Error al crear.',
+                'errors' => $e->getMessage(),
+            ], 404);
         }
     }
     public function getAll(Request $request)
--- app/Models/Mem.php
+++ app/Models/Mem.php
@@ -6,12 +6,44 @@ use Illuminate\Database\Eloquent\Model;
 
 class Mem extends Model
 {
+    public $timestamps = false;
     protected $table = 'mem_dato_fecha';
-
+    protected $fillable = [
+        'id_pem',
+        'fecha',
+        'hora',
+        'temperatura',
+        'humedad',
+        'presion',
+        'uv',
+        'precipitacion_tipo',
+        'precipitacion_probabilidad',
+        'precipitacion'
+    ]; 
     protected $hidden = [
         'fecha_registro',
         'fecha_modificacion',
         'fecha_eliminacion',
         'estado',
     ];
+
+    protected $attributes = [
+        'estado' => 'A',
+    ];
+    protected static function boot()
+    {
+        parent::boot();
+
+        static::creating(function ($model) {
+            if (!$model->fecha_registro) {
+                $model->fecha_registro = now();
+            }
+        });
+
+        static::updating(function ($model) {
+            if (!$model->fecha_modificacion) {
+                $model->fecha_modificacion = now();
+            }
+        });
+    }
 }
